792bf3f36fbd4b5300a260397145fc9db998074f
ronyshchenko <roma.onyshchenko@gmail.com>
2020-03-04 18:40:48 +0200

    Updates

---
+++ precache-manifest.ead16c0217c58fb92d197f9b14547c8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dd7866ae4b72606f5a72382f9ed8e37",
+    "url": "/ronyshchenko.github.io/react_movies-list-filter/index.html"
+  },
+  {
+    "revision": "db5e72a2f769e669da25",
+    "url": "/ronyshchenko.github.io/react_movies-list-filter/static/css/2.70d9d258.chunk.css"
+  },
+  {
+    "revision": "6fca6a9de061656228fd",
+    "url": "/ronyshchenko.github.io/react_movies-list-filter/static/css/main.9f604ba9.chunk.css"
+  },
+  {
+    "revision": "db5e72a2f769e669da25",
+    "url": "/ronyshchenko.github.io/react_movies-list-filter/static/js/2.d177a8bd.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ronyshchenko.github.io/react_movies-list-filter/static/js/2.d177a8bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6fca6a9de061656228fd",
+    "url": "/ronyshchenko.github.io/react_movies-list-filter/static/js/main.8555d8e2.chunk.js"
+  },
+  {
+    "revision": "bf4c601b1fb4598f2c6d",
+    "url": "/ronyshchenko.github.io/react_movies-list-filter/static/js/runtime-main.2405f1a9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "/ronyshchenko.github.io/react_movies-list-filter/precache-manifest.ead16c0217c58fb92d197f9b14547c8e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ronyshchenko.github.io/react_movies-list-filter/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
